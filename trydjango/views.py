1cba0a65fa72997999d24a0c67ba1a350326f4cc
mhbaando <maxamuudyare21@gmail.com>
2023-01-22 12:26:03 +0300

    another way to render template

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
-from django.template.loader import render_to_string
+from django.template.loader import render_to_string, get_template
 from articles.models import Article
 
 
 def home_view(request):
     # takes request and returns the response
     article_obj = Article.objects.get(id=1)
+
+    # one to render the template
     html_string = render_to_string('home.html', context=article_obj.__repr__())
-    return HttpResponse(html_string)
+
+    # we can also render this way
+    tmpl = get_template('home.html')
+    # pass any data here
+    tmpl_srting = tmpl.render(context=article_obj.__repr__())
+    return HttpResponse(tmpl_srting)
