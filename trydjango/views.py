ebf61c5dd77889c87618205a90b081df68ec180f
mhbaando <maxamuudyare21@gmail.com>
2023-01-22 12:22:58 +0300

    renders the template as string format

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 from articles.models import Article
 
 
 def home_view(request):
     # takes request and returns the response
     article_obj = Article.objects.get(id=1)
-    print(article_obj.__repr__())
-    return HttpResponse(f'{article_obj.content}')
+    html_string = render_to_string('home.html', context=article_obj.__repr__())
+    return HttpResponse(html_string)
