d1f64c102c720c4858180cd7957da340806cd6d0
mhbaando <maxamuudyare21@gmail.com>
2023-01-22 12:12:06 +0300

    created new app

---
+++ articles/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Article(models.Model):
+    title = models.TextField()
+    content = models.TextField()
+
+    def __repr__(self):
+        return {
+            'title': self.title,
+            'content': self.content
+        }
