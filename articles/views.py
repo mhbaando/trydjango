bc35255f7b8c40824bde050412a21b1f71b35781
mhbaando <maxamuudyare21@gmail.com>
2023-01-24 15:06:33 +0300

    implemented the detail

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from .models import Article
+
+
+def artice_detail_view(request, id):
+    article_obj = Article.objects.get(id=id)
+    context = {'article': article_obj}
+
+    return render(request, 'articles/detail.html', context=context)
