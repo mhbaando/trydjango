c6a834cfd5eca6f9ec0c1a45cf47f355c58b2017
mhbaando <maxamuudyare21@gmail.com>
2023-01-25 14:04:22 +0300

    registered url for accounts

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from .views import home_view
-from articles.views import artice_detail_view
+from articles import views
+from accounts.views import login_view
 
 urlpatterns = [
     path('', home_view),  # index url home page
-    path('articles/<int:id>/', artice_detail_view),
+    path('articles/', views.article_search_view),
+    path('articles/create/', views.article_create_view),
+    path('articles/<int:id>/', views.artice_detail_view),
+    
     path("admin/", admin.site.urls),
+    path('login/', login_view), 
+    
 ]
