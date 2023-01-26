b05e9deedcb38330e07942d893505f3444d17958
mhbaando <maxamuudyare21@gmail.com>
2023-01-26 12:27:17 +0300

    add django forms

--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from .models import Article
-
+from .forms import ArticleForm
 
 def article_search_view(request):
     # get the data from the form 
@@ -29,20 +30,55 @@ def artice_detail_view(request, id):
 
     return render(request, 'articles/detail.html', context=context)
 
+
+# @login_required
+# def article_create_view(request):
+    
+#     form = ArticleForm()
+#     context = {
+#         'article':{},
+#         'form':form
+#     }
+    
+#     if request.method == 'POST':
+#         #  pass all uncleaned data to the form
+#         form = ArticleForm(request.POST)
+        
+#         # check if the form is cleaned
+#         if form.is_valid():
+#             title =form.cleaned_data.get('title')
+#             content = form.cleaned_data.get('content')
+        
+#             # to inter new record to the db
+#             new_articel = Article.objects.create(title=title, content=content)
+
+#             context['article']=new_articel
+#             context['created']=True
+    
+    
+#     return render(request,'articles/create.html', context=context)
+
+
+
+@login_required
 def article_create_view(request):
     
+    # passes the rquest otherwise passes none 
+    form = ArticleForm(request.POST or None)
     context = {
-        'article':{}
+        'article':{},
+        'form':form
     }
-    
-    if request.method == 'POST':
-        data = request.POST
-        title = data.get('title')
-        content = data.get('content')
         
+        # check if the form is cleaned
+    if form.is_valid():
+        title =form.cleaned_data.get('title')
+        content = form.cleaned_data.get('content')
+    
         # to inter new record to the db
         new_articel = Article.objects.create(title=title, content=content)
         
+        # insted the content context
         context['article']=new_articel
         context['created']=True
     
