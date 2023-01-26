e2a58b65a347c38fe5b424fdcb6823a4b9c8cafd
mhbaando <maxamuudyare21@gmail.com>
2023-01-26 12:26:56 +0300

    added autherntication

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import authenticate,login
-from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def login_view(request):
@@ -12,19 +12,24 @@ def login_view(request):
         # authenticate user 
         user = authenticate(request, username=username, password=password)
         
+        print(user)
         if user is None:
             context={
                 'error':'Invalid Username or Password'
             }
-            print(context)
-            render(request, 'accounts/login.html',context={'error':'hehh'})
+            return render(request, 'accounts/login.html',context=context)
         
-    return render(request, 'accounts/login.html')
+        login(request,user)
+        return redirect('/')
+    return render(request, 'accounts/login.html',{})
 
 # Create your views here.
 def logou_view(request):
-    
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        logout(request)
+        return redirect('/login')
+        
+    return render(request, 'accounts/logout.html')
 
 # Create your views here.
 def register_view(request):
